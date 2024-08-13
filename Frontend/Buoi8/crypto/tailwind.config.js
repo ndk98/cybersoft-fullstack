3adef54188140dad33a54d6fa6caf90f7da71976
ndk <ndk23071998@gmail.com>
2024-08-13 23:47:39 +0700

    Update crypto

--- Frontend/Buoi8/crypto/tailwind.config.js
+++ Frontend/Buoi8/crypto/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        '2': '2 2 0%'
+      }
+    },
   },
   plugins: [
     require("flowbite/plugin")
